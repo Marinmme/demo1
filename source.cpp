ae4c3656dfa75cb97d4744f4b9dd96bbf6176ea8
Marinmme <nguyenconghainam2@gmail.com>
2021-11-08 16:02:39 +0800

    Thay doi

--- source.cpp
+++ source.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 int tong(int a, int b);
+
+
 int main()
 {
-	int a, b;
-	char phepToan;
+	int a, b, tong;
 	cout << "Nhap so nguyen 1: ";
 	cin >> a;
 	cout << "Nhap so nguyen 2: ";
 	cin >> b;
-	cout << "Chon phep toan (+,-) ";
-	cin >> phepToan;
-	if (phepToan == "+")
-		cout << "\na + b =" << tong(a, b) << "\n";
-	return(a + b);
+	cout << "\ntong  " << a + b;
+
+
+
 	system("pause");
 	return 0;
 }
