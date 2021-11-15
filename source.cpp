5c16be5917c2c403973cf51d8bbdf226244d56da
Marinmme <nguyenconghainam2@gmail.com>
2021-11-15 14:38:49 +0800

    Thay doi tong hieu

--- source.cpp
+++ source.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 using namespace std;
-int tong(int a, int b);
-
+int tong(int a, int b){
+	return a+b;
+}
+int hieu(int a, int b){
+	return a-b;
+}
 
 int main()
 {
-	int a, b, tong;
+	int a, b;
+	char phepToan;
 	cout << "Nhap so nguyen 1: ";
 	cin >> a;
 	cout << "Nhap so nguyen 2: ";
 	cin >> b;
-	cout << "\ntong  " << a + b;
+	cout << "\Chon phep toan(+,-) " ;
+	cin phepToan;
+	if(phepToan == '+')
+		cout<<"Tong="<< tong(a,b)<<endl;
+	if(phepToan == '-')
+		cout<<"Tong="<< tong(a,b)<<endl;
 
 
 
